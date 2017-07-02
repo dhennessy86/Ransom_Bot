672430a2e59d8dc0e2d362935683723c6a765399
dhennessy86 <damienhennessy@yahoo.ie>
2017-07-02 20:53:23 +0100

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-#########################################################################################################################
-#
-# Created: 02 -July -2017
-# Purpose: track Payments made to Petya hard coded bitcoin payment site
-#          Script will be called every 30 mins from crontab
-#########################################################################################################################
 
 from lxml import html
 import requests
@@ -12,6 +6,9 @@ import re
 import math
 import tweepy, time
 from credentials import *
+
+# Twitter Users to call
+Users = ['<insert user handle>']
 
 # Twitter Auth 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -21,22 +18,20 @@ api = tweepy.API(auth)
 # Define Function for comparing if value has changed
 def Compare(x,y):
 	if x==y:
-		print x," is equal to ",y
-		print "Nothing to do"
+		print ("The number of payments has not increased, still at: %s" % (x))
 	elif x>y:
-		tweetlist = ("New payment made for Petya, the total number of payments is now: %s \nTotal Amount Paid: €%s" % (No_transactions,Round_Euro) )
+		tweetlist = ("The total number of Petya payments is now: %s \nTotal Amount Paid: €%s \n%s" % (No_transactions,Round_Euro,Users) )
 		api.update_status(tweetlist)
 		print "Tweet has been sent"
 		output = open ("num.txt","w") 
 		output.write(str(x)) 
 		output.close()
 	else:
-		print x," is less than ",y 
-		print "nothing to do"
+		print ("The number of payments has not increased, still at: %s" % (x))
 		
 
 #retrieve web page
-page = requests.get('https://blockchain.info/address/12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw')
+page = requests.get('https://blockchain.info/address/12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw')  # insert new payment sites here
 tree = html.fromstring(page.content)
 
 # this will get the number of transactions, creates lists
@@ -53,10 +48,10 @@ No_BTC = re.sub(r"[^0-9.]", "", strip)  # strip out BTC value
 
 page1 = requests.get('http://api.coindesk.com/v1/bpi/currentprice/EUR.json')  # get request to get latest BTC rates in euro
 contents = page1.text													
-rates = re.findall(r"EUR\",\"rate\":\".*\",\"d", contents )	 # strip out in euro line
-rates1 = (", ".join(rates))					 # convert to string
-euro_rate = re.findall(r"[0-9]{1,},[0-9]{1,}", rates1 )		 # strip out euro value
-euro_rate1 = (", ".join(euro_rate))				 # convert to string
+rates = re.findall(r"EUR\",\"rate\":\".*\",\"d", contents )					   # strip out in euro line
+rates1 = (", ".join(rates))									   # convert to string
+euro_rate = re.findall(r"[0-9]{1,},[0-9]{1,}", rates1 )						   # strip out euro value
+euro_rate1 = (", ".join(euro_rate))								   # convert to string
 euro_rate2 = re.sub(r"[^0-9]", "", euro_rate1)
 Total_Value = float(euro_rate2) * float(No_BTC)
 Round_Euro = int(round(Total_Value))
@@ -69,4 +64,6 @@ file_contents = file.readline()
 y = int(file_contents)
 file.close()
 
+
 Compare(x,y)
+
